4f3d7b74ec68bae6a1961098dae0e6e617152d28
dchuan <dongchuan55@gmail.com>
2016-04-24 12:08:22 +0200

    Remove annotation

--- src/main/java/com/dong/web/mapper/UserMapper.java
+++ src/main/java/com/dong/web/mapper/UserMapper.java
@@ -1,11 +1,9 @@
 package com.dong.web.mapper;
 
 import com.dong.web.model.User;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface UserMapper {
 
     int getRowCount();
