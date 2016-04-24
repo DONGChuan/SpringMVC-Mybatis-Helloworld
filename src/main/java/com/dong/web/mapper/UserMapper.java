cbb50af88d123a59400f989149462eb4993f00d8
dchuan <dongchuan55@gmail.com>
2016-04-24 12:11:38 +0200

    Update sql with annotation

--- src/main/java/com/dong/web/mapper/UserMapper.java
+++ src/main/java/com/dong/web/mapper/UserMapper.java
@@ -1,13 +1,19 @@
 package com.dong.web.mapper;
 
 import com.dong.web.model.User;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 public interface UserMapper {
 
+    /**
+     * XML-based and annotation based mapper
+     */
+
     int getRowCount();
 
+    @Select("select * from tb_user")
     List<User> getAllUsers();
 
 }
